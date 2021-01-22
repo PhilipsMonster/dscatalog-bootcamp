3ab22f0b6c29d6ff36361dceb08969d4859d4503
Marcelo Gomes <marcelosuptec@hotmail.com>
2021-01-22 08:32:59 -0300

    H2 database

---
+++ backend/src/main/java/com/devsuperior/dscatalog/categories/CategoryService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dscatalog.categories;
+
+import java.util.List;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+
+        return repository.findAll();
+
+    }
+
+}
